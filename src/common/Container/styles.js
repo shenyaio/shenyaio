ea2d45606daef5ad8e391edc5b9548c768d0ac3e
Yancen Li <lee0741@gmail.com>
2021-02-28 23:22:28 +0900

    Update font style
    Signed-off-by: Yancen Li <lee0741@gmail.com>

--- src/common/Container/styles.js
+++ src/common/Container/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  font-family: 'Noto Sans SC', sans-serif;
   position: relative;
   width: 100%;
   max-width: 1280px;
